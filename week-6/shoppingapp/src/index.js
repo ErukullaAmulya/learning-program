e6d731bdf676ed7b9ab2531bd1ac8fcac1e851cc
ErukullaAmulya <22eg107c53@anurag.edu.in>
2025-07-27 19:30:53 +0530

    week-6

--- week-6/shoppingapp/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import OnlineShopping from "./OnlineShopping"; // ✅ Make sure the path matches your file name
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<OnlineShopping />);
