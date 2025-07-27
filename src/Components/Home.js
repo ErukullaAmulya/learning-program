c7e1067fcab99c6a56fdfe754c985b24fe6d6987
ErukullaAmulya <22eg107c53@anurag.edu.in>
2025-07-27 18:35:14 +0530

    week6

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class Home extends Component {
+    render() {
+        return(
+        <div>
+            <h3>Welcome to the Student Management System</h3>
+        </div>
+        );
+    }
+}
+
+export default Home;
